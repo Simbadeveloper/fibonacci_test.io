579c4737a8f7520538ab9ac0f730430474736872
simbadeveloper <silverdeltamega@gmail.com>
2018-06-12 16:58:27 +0300

    fourth commit

--- factorial/factorial.py
+++ factorial/factorial.py
@@ -1,8 +1,16 @@
-#return the factorial of the argument "number"
+#fizzbuzz checking number divisible by both 5,3
+#and both 5 and 3 in range 100
+for x in range(1,101):
 
-def factorial(n):
-   if n == 0:
-    return 1
-   else:
-    return n * factorial(n-1)
+    if x % 3==0 and x %5==0:
+        print ("FizzBuzz")
+
+    elif x%3==0:
+        print ("Fizz")
+
+    elif x%5==0:
+        print ("Buzz")
+
+    else:
+        print(x)
 
