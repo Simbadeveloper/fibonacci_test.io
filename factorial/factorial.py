2ca90e4aaff9984adc1014cf9b3c56da95ab1390
Silas Omurunga <38291725+Simbadeveloper@users.noreply.github.com>
2018-06-18 11:43:31 +0300

    Update factorial.py

--- factorial/factorial.py
+++ factorial/factorial.py
@@ -1,16 +1,11 @@
-#fizzbuzz checking number divisible by both 5,3
-#and both 5 and 3 in range 100
-for x in range(1,101):
+#factorial test
+def factorial(n):
+    ni = int(n)
+    
+  if ni !=n or ni<=0:
+    raise ValueError("%s is not a positive interger." %n)
+    
+ if ni == 1:
+    return 1
 
-    if x % 3==0 and x %5==0:
-        print ("FizzBuzz")
-
-    elif x%3==0:
-        print ("Fizz")
-
-    elif x%5==0:
-        print ("Buzz")
-
-    else:
-        print(x)
-
+return ni * factrorial(ni -1)
